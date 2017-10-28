a876692a456e2ce7ec6d5265cb3f8f00cac346e9
Ryan Do <falcsmike7@gmail.com>
2017-10-28 00:38:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (distance) {
-  if (distance === 43) { 
+  if (distance === 43) {
     return 1;
   } else {
     return 8;
   }
+}
+
+function distanceFromHqInFeet (distance) {
+  distanceFromHqInBlocks (distance) * 264;
 }
